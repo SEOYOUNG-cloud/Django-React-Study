f3304cf2207a8fafba71d213c0c7bad67e51c0d6
SEOYOUNG-cloud <67596451+SEOYOUNG-cloud@users.noreply.github.com>
2021-09-12 14:34:55 +0900

    props예제 완료

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({fav}){
+  return <h1>I like {fav}</h1>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  //return <div className="App"/>;
+  return ( // App component가 HTML 반환
+  <div>
+    <h1>Hello</h1>
+    <Food fav="김치" />
+    <Food fav="samsung"/>
+    <Food fav="coffee"/>
     </div>
   );
 }
