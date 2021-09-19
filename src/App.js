0ba5ed412dbbe25bed5ab543de84522fdd18bdd0
SEOYOUNG-cloud <67596451+SEOYOUNG-cloud@users.noreply.github.com>
2021-09-19 17:41:51 +0900

    map() 사용하기

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React from 'react';
 
-function Food({fav}){
-  return <h1>I like {fav}</h1>;
+function Food({name, picture}){
+  return (
+    <div>
+    <h2>I like {name}</h2>
+    <img src = {picture} />
+  </div>
+  );
 }
+
+
+const foodILike = [ // 데이터 입력
+  {name : 'KimChi',
+image : 'https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png',},
+{
+  name : '불고기',
+  image : 'https://recipe1.ezmember.co.kr/cache/recipe/2016/12/30/df939769792632a48a0eba8bc895e8601.jpg',
+},
+{
+  name : 'Bibimbap',
+  image : 'https://t1.daumcdn.net/liveboard/SNUH/fed8c48df21b43ada043b4cdda7dfe57.JPG',
+},
+];
 
 function App() {
   //return <div className="App"/>;
   return ( // App component가 HTML 반환
   <div>
-    <h1>Hello</h1>
-    <Food fav="김치" />
-    <Food fav="samsung"/>
-    <Food fav="coffee"/>
+    { <h1>Hello</h1> }
+   {foodILike.map(dish => (
+    <Food name={dish.name} picture={dish.image}/>
+    ))}
     </div>
   );
 }
