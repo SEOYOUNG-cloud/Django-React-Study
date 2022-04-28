0cb5703d0769b6467ddc1cac2286f1cce9c3953a
PARKSEOYOUNG <gksdidtk123@gmail.com>
2022-04-29 01:06:01 +0900

    django template, view
    django template, view 강의 완료

--- costaurant/foods/views.py
+++ costaurant/foods/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from django.http import Http404
 
 def index(request):
     today = datetime.today()
@@ -14,4 +15,15 @@ HttpResponse 객체로 반환하는 함수입니다.
 쉽게 말하면 인자로 넘겨주는 템플릿과 context 데이터를 합쳐서 HttpResponse 객체로 돌려주는 함수인 거죠.
 필수인자는 request와 template_name
 '''
+
+def food_detail(request, food): # urls에서 넘어온 변수를 food로 받음
+    context = dict()
+    if food=='chicken':
+        context['name'] = '코딩에 빠진 닭'
+        context['description'] = '주머니가 가벼워서'
+        context['price'] = 10000
+        context['img_path'] = 'foods/images/chicken.jpg'
+    else:
+        raise Http404('이런 음식은 설정 안했삼') # raise는 파이썬에서 지정한 에러 강제 발생시킴
+    return render(request, 'foods/detail.html', context=context)
 
